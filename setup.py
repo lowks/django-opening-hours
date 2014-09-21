75acf0d85cebca7760a30de64daaca81deceb781
Low Kian Seong <kianseong@gmail.com>
2014-09-21 19:31:23 +0800

    Update setup.py
    Minor pep8 touch up.

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = "django-opening-hours",
-    version = "1.4.3",
-    url = 'https://github.com/ondrejsika/django-opening-hours/',
-    license = 'MIT',
-    description = "Opening hours field and admin widget",
-    author = 'Ondrej Sika',
-    author_email = 'ondrej@ondrejsika.com',
-    packages = find_packages(),
-    requires = [],
-    include_package_data = True,
-    zip_safe = False,
+    name="django-opening-hours",
+    version="1.4.3",
+    url='https://github.com/ondrejsika/django-opening-hours/',
+    license='MIT',
+    description="Opening hours field and admin widget",
+    author='Ondrej Sika',
+    author_email='ondrej@ondrejsika.com',
+    packages=find_packages(),
+    requires=[],
+    include_package_data=True,
+    zip_safe=False,
 )
